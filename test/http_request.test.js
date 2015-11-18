cc9440748a822b1d821f015ce31c7b895e5d8fa4
Nelson <contact.nelsonic@gmail.com>
2015-11-18 23:15:09 +0000

    works. but not anywhere near 100% test coverage... :unamused:

--- test/http_request.test.js
+++ test/http_request.test.js
@@ -5,7 +5,7 @@ var cberrmsg = "please supply a callback"
 
 test('Attepmt to invoke REQUEST WITHOUT http request options (ERROR CHECK)', function(t) {
   try {
-    var result  = REQUEST(); // no options or callback! :-O
+    var result = REQUEST(); // no options or callback! :-O
   } catch (error){
     console.log(error);
     t.ok(error.indexOf(optsmsg) > -1, "*Wanted Error* Got "+error + " (as expected!)");
@@ -24,7 +24,7 @@ test('Attepmt to invoke REQUEST WITHOUT VALID callback funciton (ERROR CHECK)', 
         'Content-Type': 'application/json'
       }
     };
-    var result  = REQUEST(options);
+    var result = REQUEST(options);
   } catch (error){
     console.log(error);
     t.ok(error.indexOf(cberrmsg) > -1, "*Wanted Error* Got "+error + " (as expected!)");
--- try.js
+++ try.js
@@ -29,7 +29,7 @@ server.connection({
 var opts = {
   REDIRECT_URL: '/githubauth',  // must match google app redirect URI
   handler: require('./example/github_oauth_handler.js'), // your handler
-  scope: 'email' // profile
+  scope: 'user' // get user's profile see: developer.github.com/v3/oauth/#scopes
 };
 
 var hapi_auth_google = require('./lib');
@@ -43,8 +43,8 @@ server.route({
   method: 'GET',
   path: '/',
   handler: function(req, reply) {
-		var imgsrc = 'https://developers.google.com/accounts/images/sign-in-with-google.png';
-		var btn = '<a href="' + url +'"><img src="' +imgsrc +'" alt="Login With Google"></a>'
+		var src = 'https://cloud.githubusercontent.com/assets/194400/11214293/4e309bf2-8d38-11e5-8d46-b347b2bd242e.png';
+		var btn = '<a href="' + url + '"><img src="' + src + '" alt="Login With GitHub"></a>';
     reply(btn);
   }
 });
@@ -55,86 +55,3 @@ server.start(function(err){ // boots your server
 });
 
 module.exports = server;
-
-/*
-var https         = require('https');
-
-var redis_client  = require('redis-connection')();
-
-function make_post_data (request) {
-  return querystring.stringify({
-    client_id     : process.env.GITHUB_CLIENT_ID,
-    client_secret : process.env.GITHUB_CLIENT_SECRET,
-    code          : request.query.code
-  });
-}
-
-function make_options (post_data) {
-  return {
-    hostname: 'github.com',
-    path: '/login/oauth/access_token',
-    method: 'POST',
-    headers: {
-      'Accept'        : 'application/json',
-      'Content-Type'  : 'application/x-www-form-urlencoded',
-      'Content-Length': post_data.length
-    }
-  };
-}
-
-/* redis_login_handler stores access_token for later use
- * @param {String} access_json - the string returned by github auth
- * @param {Function} callback - call this when redis replies
- */
-
-/*
-function redis_login_handler (access_json, callback) {
-  var access_token = 'token ' + JSON.parse(access_json).access_token;
-  https.get({
-    hostname: 'api.github.com',
-    path: '/user',
-    method: 'GET',
-    headers: {
-      authorization: access_token,
-      'User-Agent': 'Tudo'
-    }
-  }, function (res){
-    access_token = access_token.split(' ')[1];
-    var user_name = '';
-    res.setEncoding('utf-8');
-    res.on('data', function (chunk){
-      user_name += chunk;
-    }).on('end', function(){
-      user_name = JSON.parse(user_name).login;
-      redis_client.hset('tokens', user_name, access_token, callback);
-    })
-  });
-}
-
-function token_response_handler (response) {
-  var json_string = '';
-  response.setEncoding('utf-8');
-  response.on('data', function (chunk){
-    json_string += chunk;
-  });
-  response.on('end', function(){redis_login_handler(json_string)});
-}
-
-function get_access_token (request, reply) {
-  var post_data = make_post_data(request);
-  var options   = make_options(post_data);
-  var req = https.request(options, token_response_handler);
-  req.write(post_data);
-  req.end();
-}
-
-function authentication_handler (request, reply) {
-  var body = riot.render(views.login);
-  reply(views.header + body + views.footer);
-  if (request.query && request.query.code) { get_access_token(request, reply); }
-}
-
-module.exports = authentication_handler;
-module.exports.redis_login_handler = redis_login_handler;
-module.exports.redis_client = redis_client;
-*/
