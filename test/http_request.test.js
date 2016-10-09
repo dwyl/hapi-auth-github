06193152f0036737a6e152ff3545e27c23590743
nelsonic <contact.nelsonic+github@gmail.com>
2016-10-09 22:44:32 +0100

    [WiP] linting /lib with goodparts update errors in /lib/http_request.js to follow no-throw-literal rule https://github.com/dwyl/goodparts/issues/108 for #26

--- test/http_request.test.js
+++ test/http_request.test.js
@@ -1,19 +1,19 @@
 var test     = require('tape');
 var REQUEST  = require('../lib/http_request');
-var optsmsg  = "requires valid http request options";
-var cberrmsg = "please supply a callback"
+var optsmsg  = 'requires valid http request options';
+var cberrmsg = 'callback required as second param'
 
 test('Attepmt to invoke REQUEST WITHOUT http request options (ERROR CHECK)', function(t) {
   try {
     var result = REQUEST(); // no options or callback! :-O
   } catch (error){
-    console.log(error);
-    t.ok(error.indexOf(optsmsg) > -1, "*Wanted Error* Got "+error + " (as expected!)");
+    // console.log('>>> ', error.message);
+    t.ok(error.message.indexOf(optsmsg) > -1, '*Wanted Error* Got ' + error + ' (as expected!)');
     t.end();
   }
 })
 
-test('Attepmt to invoke REQUEST WITHOUT VALID callback funciton (ERROR CHECK)', function(t) {
+test('Attepmt to invoke REQUEST WITHOUT VALID callback function (ERROR CHECK)', function(t) {
   try {
     var options = {
       hostname: '127.0.0.1', // gets over-written below if using HEROKU
@@ -26,13 +26,13 @@ test('Attepmt to invoke REQUEST WITHOUT VALID callback funciton (ERROR CHECK)', 
     };
     var result = REQUEST(options);
   } catch (error){
-    console.log(error);
-    t.ok(error.indexOf(cberrmsg) > -1, "*Wanted Error* Got "+error + " (as expected!)");
+    // console.log(error);
+    t.ok(error.message.indexOf(cberrmsg) > -1, '*Wanted Error* Got '+error + ' (as expected!)');
     t.end();
   }
 })
 
-test('Force req.on("error") condition in http request', function(t) {
+test('Force req.on(`error`) condition in http request', function(t) {
   var options = {
     hostname: '127.0.0.1', // gets over-written below if using HEROKU
     port: 8000,        // also over-written below if using HEROKU
@@ -44,7 +44,7 @@ test('Force req.on("error") condition in http request', function(t) {
   };
   REQUEST(options, function(error) {
     // console.log(error);
-    t.ok(error.code === 'ECONNREFUSED', "*Wanted Error* Got "+error.code + " (as expected!)");
+    t.ok(error.code === 'ECONNREFUSED', '*Wanted Error* Got '+error.code + ' (as expected!)');
     t.end();
   });
 })
